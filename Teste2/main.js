4c4cc3dcad0bf89cd1c8bf33d637e54b758874bf
elvis-rodrigues7 <116103185+elvis-rodrigues7@users.noreply.github.com>
2022-10-31 17:00:25 -0300

    Add files via upload

---
+++ Teste2/main.js
@@ -0,0 +1,6 @@
+let im1 = document.querySelector('.img1')
+let im2 = document.querySelector('.img2')
+let im3 = document.querySelector('.img3')
+let im4 = document.querySelector('.img4')
+
+let but = document.querySelector('botao')
